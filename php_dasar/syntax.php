07ac4faef182c0cd69939f81dc455d2c20ac17c1
Aldhi <aldhi.albadri@gmail.com>
2024-10-07 21:10:33 +0700

    add materi

---
+++ php_dasar/syntax.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<?php 
+
+$warna = "blue";
+
+?>
+<body style="background-color: <?php echo $warna ?>;">
+    <h1>Hello</h1>
+
+    <h1>
+        <?php 
+            echo "Hello PHP"; 
+        ?>
+    </h1>
+
+    <?php 
+        echo "<h1>Hello Sytax HTML</h1>";
+    ?>
+
+    
+</body>
+</html>
---
+++ php_dasar/variable_general.php
@@ -0,0 +1,7 @@
+<?php 
+
+$nama = "budi";
+
+echo $nama;
+
+?>
