484f29addb01e53e2bae92afe122e200293f875a
dumaria <riaduma67@gmail.com>
2024-10-14 20:55:49 +0700

    soal4

---
+++ php_dasar/quiz/2255201004/soal44.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION['status'])) {
+    
+    echo "Status session: " . $_SESSION['status'];
+} else {
+    
+    echo "Session status belum diatur.";
+}
+?>
